637c1b7fd0a869049ce6f400fc4bf6cf2f0afa20
avanishdixit <dixitavanish1999@gmail.com>
2020-12-26 19:37:01 +0530

    inline css

--- src/index.js
+++ src/index.js
@@ -4,11 +4,17 @@ import Card from "./Card";
 import App from "./App";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
- 
+const heading={
+  color:'red',
+  textTransfer:"capitalize",
+  textAlign:"center",
+  margin:"70px 0",
+  textShadow:"0px 2px 4px #ffe9c5"
+} ;
 
 ReactDOM.render(
   <>
-  <App/>
+  <h1 style={heading}>My name is khan</h1>
   </>,
   document.getElementById("root")
 
